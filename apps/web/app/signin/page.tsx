1ae780fbae556c18bdc60aad7ebd36777374b9fb
Jake Wang <jakewangzhi@gmail.com>
2024-07-26 21:39:06 -0500

    make sure both button have the same width

--- apps/web/app/signin/page.tsx
+++ apps/web/app/signin/page.tsx
@@ -2,13 +2,11 @@
 
 import { AddToHomeScreenBanner } from "~/components/AddToHomeScreenBanner";
 import { Button } from "~/components/Button";
-import { Typography, useTheme } from "@mui/material";
+import { Typography } from "@mui/material";
 import { signIn } from "next-auth/react";
 import { useCallback } from "react";
 
 export default function SignInPage() {
-  const theme = useTheme();
-
   const handleLinkedinClick = useCallback(
     () => signIn("linkedin", { callbackUrl: "/dashboard" }),
     []
@@ -32,29 +30,11 @@ export default function SignInPage() {
         reach your goals
       </Typography>
       <div className="flex justify-center !mt-24 flex-col gap-8">
-        <Button
-          variant="contained"
-          sx={{
-            width: "200px",
-            [theme.breakpoints.up("sm")]: {
-              width: "300px",
-            },
-          }}
-          onClick={handleLinkedinClick}
-        >
+        <Button variant="contained" onClick={handleLinkedinClick}>
           Sign in with LinkedIn
         </Button>
-        <Button
-          variant="contained"
-          sx={{
-            width: "200px",
-            [theme.breakpoints.up("sm")]: {
-              width: "300px",
-            },
-          }}
-          onClick={handleGoogleClick}
-        >
-          Sign in with Google
+        <Button variant="contained" onClick={handleGoogleClick}>
+          &nbsp;Sign in with Google&nbsp;
         </Button>
       </div>
       <AddToHomeScreenBanner addBottomPadding={false} />
