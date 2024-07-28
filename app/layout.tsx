79ad18ebee75f6adc8966ce128ef14d7f8ad6d6e
Jake Wang <jakewangzhi@gmail.com>
2024-07-28 14:52:23 -0500

    use jsdelivr CDN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-sync-scripts */
 import "tailwindcss/tailwind.css";
 import "@fontsource/metropolis";
 import { Session } from "next-auth";
@@ -33,51 +35,13 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-touch-fullscreen" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="PWA Splash" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <link
           rel="manifest"
           crossOrigin="use-credentials"
           href="/manifest.json"
         />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-640x1136.png"
-          media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-750x1294.png"
-          media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-1242x2148.png"
-          media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-1125x2436.png"
-          media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-1536x2048.png"
-          media="screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-1668x2224.png"
-          media="screen and (min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="images/splash/launch-2048x2732.png"
-          media="screen and (min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
-        />
+        <script src="https://cdn.jsdelivr.net/npm/ios-pwa-splash@1.0.0/cdn.min.js"></script>
+        <script>iosPWASplash('icons/icon-512x512.png', '#0F1A24');</script>
       </head>
       <body className="font-sans bg-dark-blue mx-auto max-w-lg md:max-w-xl lg:max-w-3xl">
         <QCProvider>
