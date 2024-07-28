3d395ae4b934f92d0b0d9d96a7e147a02b824c08
Jake-WangZhi <68337589+Jake-WangZhi@users.noreply.github.com>
2024-07-28 17:17:06 -0500

    New login page update (#1)
    * modify the login page
    
    * reinstall @phosphor-icons/react
    
    * update layout
    
    * update layout
    
    * remove A2HS
    
    * add padding to the top
    
    * adjusting paddings
    
    * add flex-1
    
    * add max height
    
    * remove max height
    
    * add more padding above login section
    
    * remove padding above

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { AddToHomeScreenBanner } from "@/components/AddToHomeScreenBanner";
-import { Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { ClipLoader } from "react-spinners";
 
 // Prisma does not support Edge without the Data Proxy currently
 export const runtime = "nodejs"; // default
@@ -18,18 +17,8 @@ export default function Home() {
   }, [router]);
 
   return (
-    <main className="relative flex flex-col justify-center px-8 space-y-4 mt-20">
-      <h1 className="flex flex-col text-light-blue text-start text-5xl font-bold tracking-tight leading-72 md:text-7xl">
-        <span className="text-3xl md:text-5xl leading-48">Lil</span>
-        <span className="text-white">Networking</span>
-        App
-      </h1>
-      <div className="w-1/12 h-[202px] border-l-4 border-light-blue"></div>
-      <Typography variant="h3">
-        Build networking habits & <br />
-        reach your goals
-      </Typography>
-      <AddToHomeScreenBanner addBottomPadding={false} />
+    <main className="flex items-center justify-center h-screen">
+      <ClipLoader color="#38ACE2" size={50} />
     </main>
   );
 }
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,6 +5,9 @@ import { Button } from "@/components/Button";
 import { Typography } from "@mui/material";
 import { signIn } from "next-auth/react";
 import { useCallback } from "react";
+import { ArrowsClockwise, Notepad, UsersThree } from "@phosphor-icons/react";
+import logo from "@/public/icons/logo.png";
+import Image from "next/image";
 
 export default function SignInPage() {
   const handleLinkedinClick = useCallback(
@@ -18,18 +21,28 @@ export default function SignInPage() {
   );
 
   return (
-    <main className="relative flex flex-col justify-center px-8 space-y-4 pt-20">
-      <h1 className="flex flex-col text-light-blue text-start text-5xl font-bold tracking-tight leading-72 md:text-7xl">
-        <span className="text-3xl md:text-5xl leading-48">Lil</span>
-        <span className="text-white">Networking</span>
-        App
-      </h1>
-      <div className="w-1/12 h-[202px] border-l-4 border-light-blue"></div>
-      <Typography variant="h3">
-        Build networking habits & <br />
-        reach your goals
-      </Typography>
-      <div className="flex justify-center !mt-24 flex-col gap-8">
+    <main className="relative flex flex-1 flex-col justify-center items-center h-screen gap-14">
+      <div className="flex flex-col items-center">
+        <Image src={logo} alt="Logo" width={186} height={186} />
+        <Typography variant="h2">Lil&apos; Networking App</Typography>
+      </div>
+
+      <div className="space-y-6">
+        <div className="flex space-x-4 items-center">
+          <Notepad color="white" size={32} />
+          <Typography variant="h3">Build Networking Habits</Typography>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <UsersThree color="white" size={32} />
+          <Typography variant="h3">Maintain Relationships</Typography>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <ArrowsClockwise color="white" size={32} />
+          <Typography variant="h3">Stay Connected</Typography>
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-col mt-16 gap-8">
         <Button variant="contained" onClick={handleLinkedinClick}>
           Sign in with LinkedIn
         </Button>
