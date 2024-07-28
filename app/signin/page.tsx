5337abffda113f0bbde6bed1efec9eb614e78b96
Jake Wang <jakewangzhi@gmail.com>
2024-07-28 16:13:20 -0500

    modify the login page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,6 +5,9 @@ import { Button } from "@/components/Button";
 import { Typography } from "@mui/material";
 import { signIn } from "next-auth/react";
 import { useCallback } from "react";
+import { ArrowsClockwise, Notepad, UsersThree } from "phosphor-react";
+import logo from "@/public/icons/logo.png";
+import Image from "next/image";
 
 export default function SignInPage() {
   const handleLinkedinClick = useCallback(
@@ -18,18 +21,26 @@ export default function SignInPage() {
   );
 
   return (
-    <main className="relative flex flex-col justify-center px-8 space-y-4 pt-20">
-      <h1 className="flex flex-col text-light-blue text-start text-5xl font-bold tracking-tight leading-72 md:text-7xl">
-        <span className="text-3xl md:text-5xl leading-48">Lil</span>
-        <span className="text-white">Networking</span>
-        App
-      </h1>
-      <div className="w-1/12 h-[202px] border-l-4 border-light-blue"></div>
-      <Typography variant="h3">
-        Build networking habits & <br />
-        reach your goals
-      </Typography>
-      <div className="flex justify-center !mt-24 flex-col gap-8">
+    <main className="relative flex flex-col justify-center items-center px-8 pt-20 h-screen">
+      <Image src={logo} alt="Logo" width={186} height={186} />
+      <Typography variant="h2">Lil&apos; Networking App</Typography>
+
+      <div className="mt-14 space-y-6">
+        <div className="flex space-x-4 items-center">
+          <Notepad color="white" size={32} />
+          <Typography variant="h3">Build Networking Habits</Typography>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <UsersThree color="white" size={32} />
+          <Typography variant="h3">Maintain Relationships</Typography>
+        </div>
+        <div className="flex space-x-4 items-center">
+          <ArrowsClockwise color="white" size={32} />
+          <Typography variant="h3">Stay Connected</Typography>
+        </div>
+      </div>
+
+      <div className="flex justify-center mt-32 flex-col gap-8">
         <Button variant="contained" onClick={handleLinkedinClick}>
           Sign in with LinkedIn
         </Button>
