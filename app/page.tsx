fd77fcf1af5b67c251e6de55d76fb9a2d82b5b18
Jake-WangZhi <68337589+Jake-WangZhi@users.noreply.github.com>
2024-07-28 19:26:43 -0500

    Add homepage (#2)
    * modify the login page
    
    * reinstall @phosphor-icons/react
    
    * update layout
    
    * update layout
    
    * remove A2HS
    
    * add padding to the top
    
    * adjusting paddings
    
    * add flex-1
    
    * add max height
    
    * remove max height
    
    * add more padding above login section
    
    * remove padding above
    
    * add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { ClipLoader } from "react-spinners";
+import { Heart } from "@phosphor-icons/react";
+import logo from "@/public/icons/logo.png";
+import Image from "next/image";
 
 // Prisma does not support Edge without the Data Proxy currently
 export const runtime = "nodejs"; // default
@@ -17,8 +21,18 @@ export default function Home() {
   }, [router]);
 
   return (
-    <main className="flex items-center justify-center h-screen">
-      <ClipLoader color="#38ACE2" size={50} />
+    <main className="relative flex flex-col items-center h-screen justify-center">
+      <div className="flex flex-col items-center">
+        <Image src={logo} alt="Logo" width={186} height={186} />
+        <Typography variant="h2">Lil&apos; Networking App</Typography>
+      </div>
+      <div className="my-4">
+        <ClipLoader color="#38ACE2" size={50} />
+      </div>
+      <div className="absolute bottom-0 mb-4 flex items-center gap-2">
+        <Typography variant="subtitle1">Made with</Typography>
+        <Heart color="red" size={24} weight="fill" />
+      </div>
     </main>
   );
 }
