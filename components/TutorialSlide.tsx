dc5cb0258e8aa7dc19e0b7cd63853718bddf7337
Jake Wang <jakewangzhi@gmail.com>
2024-08-01 04:21:44 -0500

    make tutorial modal more responsive

--- components/TutorialSlide.tsx
+++ components/TutorialSlide.tsx
@@ -13,17 +13,21 @@ export const TutorialSlide = ({ slide }: Props) => {
     Math.floor(getVisibleWidth(useWindowWidth()) * 0.9) - 32;
 
   return (
-    <div className="space-y-2 flex flex-col justify-center items-center">
+    <div className="space-y-6 flex flex-col justify-center items-center">
       <div
         className="h-[186px] bg-dark-blue flex justify-center items-center"
         style={{ width: `${containerWidth}px` }}
       >
         <Image alt={slide.title} src={slide.image} />
       </div>
-      <Typography variant="h2">{slide.title}</Typography>
-      <Typography variant="subtitle1" sx={{ textAlign: "center" }}>
-        {slide.description}
-      </Typography>
+      <div className="space-y-2">
+        <Typography variant="h2" sx={{ textAlign: "center" }}>
+          {slide.title}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ textAlign: "center" }}>
+          {slide.description}
+        </Typography>
+      </div>
     </div>
   );
 };
