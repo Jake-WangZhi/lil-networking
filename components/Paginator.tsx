058890f1a5525879ca95f80aab30a6962100dc38
Jake Wang <jakewangzhi@gmail.com>
2024-08-01 05:31:06 -0500

    make dot clickable

--- components/Paginator.tsx
+++ components/Paginator.tsx
@@ -1,15 +1,17 @@
 interface Props {
   activeIndex: number;
   numDots: number;
+  onDotClick: (index: number) => void;
 }
 
-export const Paginator = ({ activeIndex, numDots }: Props) => {
+export const Paginator = ({ activeIndex, numDots, onDotClick }: Props) => {
   return (
     <div className="flex justify-center space-x-2 mt-4">
       {Array.from({ length: numDots }).map((_, index) => (
         <span
           key={index}
-          className={`block ${
+          onClick={() => onDotClick(index)}
+          className={`block cursor-pointer ${
             index === activeIndex
               ? "w-6 h-3 bg-light-blue border rounded-lg border-light-blue"
               : "w-3 h-3 bg-[rgba(255,255,255,0.7)] rounded-full"
