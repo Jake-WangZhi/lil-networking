e70d50a97e428e18b0c17c387ba765b1a008cb78
Jake Wang <jakewangzhi@gmail.com>
2024-08-01 18:15:44 -0500

    fix onboarding styling issues

--- components/OnboardingIntroPage.tsx
+++ components/OnboardingIntroPage.tsx
@@ -18,17 +18,21 @@ export const OnboardingIntroPage = ({
   handleNextClick,
 }: Props) => {
   return (
-    <div className="bg-dark-blue">
-      <div
-        className={`flex flex-col h-[400px] xs:h-[475px] lg:h-[650px] justify-end ${
-          addImgPadding && "px-8 md:px-10 lg:px-12"
-        }`}
-      >
-        <Image src={image} alt={title} className="w-full" />
-      </div>
-      <div className="px-6 text-white mt-12 space-y-6">
-        <Typography variant="h1">{title}</Typography>
-        <Typography variant="h3">{description}</Typography>
+    <div className="bg-dark-blue space-y-[72px] lg:space-y-9">
+      <div>
+        <div className="flex flex-col items-center">
+          <div
+            className={`flex h-[400px] items-end xs:h-[475px] lg:h-[525px] lg:w-[525px] ${
+              addImgPadding && "px-8 md:px-10 lg:px-12"
+            }`}
+          >
+            <Image src={image} alt={title} className="w-full" />
+          </div>
+        </div>
+        <div className="px-6 text-white mt-12 space-y-6 h-40">
+          <Typography variant="h1">{title}</Typography>
+          <Typography variant="h3">{description}</Typography>
+        </div>
       </div>
       <div className="flex justify-center mt-6">
         <Button
