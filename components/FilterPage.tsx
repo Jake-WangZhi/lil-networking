4e0555fdcc39c9aa1e48cb1b095a8179577748b7
Jake Wang <jakewangzhi@gmail.com>
2024-08-18 01:55:40 -0500

    clear all won't change selected tags

--- components/FilterPage.tsx
+++ components/FilterPage.tsx
@@ -43,7 +43,6 @@ export const FilterPage = ({
 
   const handleClearClick = () => {
     setClickedTags((prev) => prev.map(() => false));
-    setSelectedTags(clickedTags.map(() => ""));
   };
 
   const handleConfirmClick = () => {
