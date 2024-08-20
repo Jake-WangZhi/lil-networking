d9991828806c7ca4cca944d90e2036648bf1f34e
Jake Wang <jakewangzhi@gmail.com>
2024-08-20 00:22:13 -0500

    add author tag

--- app/(main)/settings/page.tsx
+++ app/(main)/settings/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/Button";
 import { NavFooter } from "@/components/NavFooter";
 import { Avatar, Typography } from "@mui/material";
-import { UserCircle, CaretRight, SignOut } from "@phosphor-icons/react";
+import { UserCircle, CaretRight, SignOut, Heart } from "@phosphor-icons/react";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 import { useUser } from "@/contexts/UserContext";
@@ -29,78 +29,96 @@ export default function SettingsPage() {
   const handleLogoutClick = useCallback(() => signOut(), []);
 
   return (
-    <main className="relative min-h-screen pt-4 pb-8 space-y-6 md:pt-8">
-      <Typography variant="h1" sx={{ px: "16px" }}>
-        Settings
-      </Typography>
-      <div className="flex items-center px-4 space-x-2">
-        <div className="w-10 h-10 bg-white bg-opacity-[0.12] rounded-full flex justify-center items-center md:w-11 md:h-11 lg:w-12 lg:h-12">
-          {image ? (
-            <Avatar alt="Remy Sharp" src={image} />
-          ) : (
-            <UserCircle
-              color="white"
-              size={24}
-              className="md:w-7 md:h-7 lg:w-8 lg:h-8"
-            />
-          )}
-        </div>
-        <Typography variant="h3" sx={{ fontWeight: 600 }}>
-          {name}
+    <main className="relative min-h-screen pt-4 pb-8 flex flex-col justify-between md:pt-8">
+      <div className="space-y-6">
+        <Typography variant="h1" sx={{ px: "16px" }}>
+          Settings
         </Typography>
+        <div className="flex items-center px-4 space-x-2">
+          <div className="w-10 h-10 bg-white bg-opacity-[0.12] rounded-full flex justify-center items-center md:w-11 md:h-11 lg:w-12 lg:h-12">
+            {image ? (
+              <Avatar alt="Remy Sharp" src={image} />
+            ) : (
+              <UserCircle
+                color="white"
+                size={24}
+                className="md:w-7 md:h-7 lg:w-8 lg:h-8"
+              />
+            )}
+          </div>
+          <Typography variant="h3" sx={{ fontWeight: 600 }}>
+            {name}
+          </Typography>
+        </div>
+        <div className="space-y-4">
+          <Button
+            variant="text"
+            sx={{ width: "100%" }}
+            onClick={handleGoalsClick}
+          >
+            <div className="flex justify-between px-4 w-full">
+              <Typography variant="subtitle1">Goals</Typography>
+              <CaretRight size={24} className="md:w-7 md:h-7 lg:w-8 lg:h-8" />
+            </div>
+          </Button>
+          <Button
+            variant="text"
+            sx={{
+              width: "100%",
+            }}
+            onClick={handleNotificationsClick}
+          >
+            <div className="flex justify-between px-4 w-full">
+              <Typography variant="subtitle1">Notifications</Typography>
+              <CaretRight size={24} className="md:w-7 md:h-7 lg:w-8 lg:h-8" />
+            </div>
+          </Button>
+          <Button
+            variant="text"
+            sx={{
+              width: "100%",
+            }}
+            onClick={handleTutorialsClick}
+          >
+            <div className="flex justify-between px-4 w-full">
+              <Typography variant="subtitle1">Tutorials</Typography>
+              <CaretRight size={24} className="md:w-7 md:h-7 lg:w-8 lg:h-8" />
+            </div>
+          </Button>
+          <Button
+            variant="text"
+            sx={{
+              width: "100%",
+            }}
+            onClick={handleLogoutClick}
+          >
+            <div className="flex justify-between px-4 w-full">
+              <Typography variant="subtitle1" sx={{ color: "#38ACE2" }}>
+                Logout
+              </Typography>
+              <SignOut
+                size={24}
+                className="text-light-blue md:w-7 md:h-7 lg:w-8 lg:h-8"
+              />
+            </div>
+          </Button>
+        </div>
       </div>
-      <div className="space-y-4">
-        <Button
-          variant="text"
-          sx={{ width: "100%" }}
-          onClick={handleGoalsClick}
-        >
-          <div className="flex justify-between px-4 w-full">
-            <Typography variant="subtitle1">Goals</Typography>
-            <CaretRight size={24} className="md:w-7 md:h-7 lg:w-8 lg:h-8" />
-          </div>
-        </Button>
-        <Button
-          variant="text"
-          sx={{
-            width: "100%",
-          }}
-          onClick={handleNotificationsClick}
-        >
-          <div className="flex justify-between px-4 w-full">
-            <Typography variant="subtitle1">Notifications</Typography>
-            <CaretRight size={24} className="md:w-7 md:h-7 lg:w-8 lg:h-8" />
-          </div>
-        </Button>
-        <Button
-          variant="text"
-          sx={{
-            width: "100%",
-          }}
-          onClick={handleTutorialsClick}
-        >
-          <div className="flex justify-between px-4 w-full">
-            <Typography variant="subtitle1">Tutorials</Typography>
-            <CaretRight size={24} className="md:w-7 md:h-7 lg:w-8 lg:h-8" />
-          </div>
-        </Button>
-        <Button
-          variant="text"
-          sx={{
-            width: "100%",
-          }}
-          onClick={handleLogoutClick}
-        >
-          <div className="flex justify-between px-4 w-full">
-            <Typography variant="subtitle1" sx={{ color: "#38ACE2" }}>
-              Logout
-            </Typography>
-            <SignOut
-              size={24}
-              className="text-light-blue md:w-7 md:h-7 lg:w-8 lg:h-8"
-            />
-          </div>
-        </Button>
+      <div className="mb-16 flex flex-col items-center gap-2 justify-center">
+        <Typography variant="body1" sx={{ opacity: 0.7 }}>
+          Powered by Jake Wang
+        </Typography>
+        <div className="flex items-center gap-2">
+          <Typography variant="body1" sx={{ opacity: 0.7 }}>
+            Made with
+          </Typography>
+          <Heart
+            color="red"
+            size={16}
+            weight="fill"
+            className="mb-1 md:w-5 md:h-5 lg:w-6 lg:h-6"
+          />
+        </div>
       </div>
       <NavFooter />
     </main>
